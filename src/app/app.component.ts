4e39efe51be45eff5c794a9679540bde41572467
Nikhil bari <nikbari09@gmail.com>
2024-02-16 16:41:43 +0530

    complete code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'crud_operation';
+  empdisplayForm!:any;
+
+  displayedColumns: string[] = ['id', 'firstname', 'lastname', 'email','dob','gender','action'];
+  dataSource !: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
+  constructor(private _dialog:MatDialog, private _empService:EmployeeService){ 
+
+  }
+  ngOnInit(): void {
+   this.empdisplayForm=this.getEmployeeList();
+  }
+
+  openAddEditEmpForm() {
+    const dialogRef = this._dialog.open(EmpAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next: val => {
+        if(val){
+          this.getEmployeeList();
+        }
+      },
+      error: err => console.log(err)
+    })
+  }
+
+  getEmployeeList(){
+    this._empService.getEmployee().subscribe({
+      next: (res)=>{
+       this.dataSource=new MatTableDataSource(res);
+       this.dataSource.sort=this.sort;
+       this.dataSource.paginator=this.paginator;
+        
+      },
+      error: (err)=>{
+        console.log(err);
+        
+      },
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  } 
+
+  
+
+  onDelete(id:string){
+    this._empService.deleteEmployee(id).subscribe({
+      next: (res)=>{
+        alert('Employee deleted successfully...');
+        this.getEmployeeList();
+      },
+      error: (err)=>{
+        console.log(err);
+        
+      }
+    })
+  }
+
+  openEditForm(data:any){
+    const DialogRef =this._dialog.open(EmpAddEditComponent,{
+      data,
+    });
+
+    DialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if(val) {
+            this.getEmployeeList();
+        }
+      }
+    })
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatRadioModule} from '@angular/material/radio';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpAddEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatButtonModule, MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
